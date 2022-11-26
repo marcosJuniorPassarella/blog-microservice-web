9c89567b593973f8ddc3a391ebad8bc79da82154
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-11-26 15:04:19 -0300

    Added new updates in the PostCreate component

--- client/src/PostCreate.tsx
+++ client/src/PostCreate.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { Microservices } from "./models/enums/Microservices";
 
 export default function PostCreate() {
+  const [title, setTile] = useState("");
+
+  async function onSubmit(e: any) {
+    e.preventDefault();
+    await axios.post(`${Microservices.urlPostsMicroservice}/posts`, {
+      title,
+    });
+    setTile("");
+  }
+
   return (
     <div className="">
-      <form action="">
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="">Title</label>
-          <input type="text" className="form-control" />
+          <input
+            value={title}
+            onChange={(e) => setTile(e.target.value)}
+            type="text"
+            className="form-control"
+          />
         </div>
-        <button className="btn btn-primary">Submit</button>
+        <button className="btn btn-primary mt-3">Submit</button>
       </form>
     </div>
   );
