c5ac17032b6a540b68e540e0020c704ba8fefc2d
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-11-26 14:27:14 -0300

    Created PostCreate component

---
+++ client/src/PostCreate.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function PostCreate() {
+  return (
+    <div className="">
+      <form action="">
+        <div className="form-group">
+          <label htmlFor="">Title</label>
+          <input type="text" className="form-control" />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
