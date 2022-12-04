859daeec5b14feea19838291705f4206afe9ba2d
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-04 13:49:06 -0300

    Added new updates

--- posts/index.ts
+++ posts/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { randomBytes } from 'crypto';
 import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 const port = 4000;
@@ -14,13 +15,20 @@ app.get('/posts', (req, res) => {
     res.send(posts)
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
     posts[id] = {
         id, title
     };
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    })
 
     res.status(201).send(posts[id]);
 });
